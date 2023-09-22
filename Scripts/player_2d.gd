57764e7196f4d912bd178cf7a8d4334229f9a1f6
Isaac RF <IsaacRF239@gmail.com>
2023-09-23 01:03:15 +0200

    Add Player's CharacterBody and Script
    Add CharacterBody2D, Collision and Script to the player
    
    - Also sort better the different elements in folders

---
+++ Scripts/player_2d.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+	velocity.x = 0
+	velocity.y = 0
+	
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= 1		
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += 1	
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= 1
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += 1
+		
+	velocity *= SPEED
+	
+	move_and_slide()
